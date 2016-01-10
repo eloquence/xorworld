5079eba3c1e69b3c8a6b330f6bcc703d84195530
Erik Moeller <eloquence@gmail.com>
2016-01-09 21:12:28 -0800

    Add support for XOR in either direction

--- xorworld.js
+++ xorworld.js
@@ -1,17 +1,18 @@
 (function() {
   'use strict';
   // Initial settings
-  var direction = 1;        // for tracking zoom direction
-  var zoomy = false;        // for autozoom feature
-  var paused = false;       // for pausing animation
-  var lastTime = 0;         // for calculating elapsed time between frames
-  var delay = 100;          // default wait between frames
+  var direction = 1; // for tracking zoom direction
+  var flip = 0; // whether to XOR to the right (1) or to the left (0)
+  var zoomy = false; // for autozoom feature
+  var paused = false; // for pausing animation
+  var lastTime = 0; // for calculating elapsed time between frames
+  var delay = 100; // default wait between frames
   var isRandomized = false; // we like to track artificial grids ;-)
-  var gen = 0;              // generation counter
-  var init = true;          // for tracking render initialization
-  var once = false;         // for single redraws when paused
-  var mono = false;         // if we just care about black and white
-  var gridSize = 12000;     // number of bits
+  var gen = 0; // generation counter
+  var init = true; // for tracking render initialization
+  var once = false; // for single redraws when paused
+  var mono = false; // if we just care about black and white
+  var gridSize = 12000; // number of bits
 
   // We'll retain a number of grids, so we can cycle through colors for
   // cells that keep their value across generations, and so we can walk
@@ -45,15 +46,20 @@
     if (grid.length === memory)
       grid.pop();
 
-    grid.unshift(grid[0].slice(0));  // Make a copy to manipulate
+    grid.unshift(grid[0].slice(0)); // Make a copy to manipulate
 
     for (var c = 0; c < gridSize; c++) {
       var x;
       var leftNeighbor = c === 0 ? gridSize - 1 : c - 1;
-      if (grid[0][leftNeighbor] === undefined)
+      var rightNeighbor = c === gridSize - 1 ? 0 : c + 1;
+      var neighbor;
+      neighbor = flip ? rightNeighbor : leftNeighbor;
+
+      if (grid[0][neighbor] === undefined)
         x = 1;
       else
-        x = grid[0][leftNeighbor] ^ grid[0][c];
+        x = grid[0][neighbor] ^ grid[0][c];
+
       grid[0][c] = x;
     }
     gen++;
@@ -90,7 +96,7 @@
         var color = getColor(gpos);
         particles.colors.push(color);
         xpos += 5;
-        gpos ++;
+        gpos++;
       }
       ypos -= 10;
       xpos = -500;
@@ -124,7 +130,7 @@
       return newval ? new THREE.Color(0xffffff) : new THREE.Color(0x000000);
 
     var color = 50;
-    for(var i=1;i<grid.length;i++) {
+    for (var i = 1; i < grid.length; i++) {
       var oldval = grid[i][pos];
       if (oldval === newval)
         color += 10;
@@ -132,10 +138,10 @@
         break;
     }
     if (grid[0][pos])
-      return new THREE.Color('rgb(0,0,'+color.toString()+')');
+      return new THREE.Color('rgb(0,0,' + color.toString() + ')');
     else
-      return new THREE.Color('rgb('+color.toString()+',0,0)');
-}
+      return new THREE.Color('rgb(' + color.toString() + ',0,0)');
+  }
 
   function setupClickHandlers() {
     $('#faster').click(function(e) {
@@ -172,7 +178,7 @@
     $('#randomize').click(function(e) {
       resetGrid();
       updateGrid();
-      for(var i=0;i<gridSize;i++)
+      for (var i = 0; i < gridSize; i++)
         grid[0][i] = Math.round(Math.random());
       gen = 1;
       $('#randomized').text(' (randomized)');
@@ -218,6 +224,16 @@
         $('body').append('<iframe id="music" width="100%" hidden scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1245420&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>');
       else
         $('#music').remove();
+    });
+
+    $('#flip').click(function(e) {
+      flip = !flip;
+      $('#flip').toggleClass('toggled');
+    });
+
+    $('#reset').click(function(e) {
+      once = true;
+      resetGrid();
     });
 
   }
