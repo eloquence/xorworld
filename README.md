ef9944b25d3e997d71d5449ac34459ce0f9e8b7f
Erik Moeller <eloquence@gmail.com>
2016-01-09 20:10:46 -0800

    Track state, add colors.

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # xorworld
 
 A tiny three.js experiment. No random numbers are involved (unless you click randomize!).
+
 The grid is constructed initially with each value set to 1. Then, every cell
 is XOR'd to its left neighbor. This operation is run repeatedly, and
 complexity emerges - similar to cellular automata, but the operation is carried
 forward cell by cell, rather than being applied against the whole grid in its
 previous state.
 
+The colors simply highlight cells that haven't changed state for a while. We retain
+a history of up to 20 previous states.
+
 Play with it at https://eloquence.github.io/xorworld - uses WebGL and local storage,
 tested w/ desktop Firefox and Chrome.
+
+The music, if you turn it on, is the [Alice Band Mix of "Life in Mono"](https://soundcloud.com/jzerenidis/mono-formica-blues-11-life-in-mono-alice-band-mix).
--- xorworld.js
+++ xorworld.js
@@ -10,9 +10,16 @@
   var gen = 0;              // generation counter
   var init = true;          // for tracking render initialization
   var once = false;         // for single redraws when paused
-
+  var mono = false;         // if we just care about black and white
   var gridSize = 12000;     // number of bits
-  var grid = [];            // grid array
+
+  // We'll retain a number of grids, so we can cycle through colors for
+  // cells that keep their value across generations, and so we can walk
+  // back and forward in time a bit.
+  var grid;
+  var memory = 20;
+  resetGrid();
+
   // Shared between particle system updates/inits for convenience
   var particles;
   var particleSystem;
@@ -28,26 +35,35 @@
   setupClickHandlers();
   render();
 
+  function resetGrid() {
+    grid = new Array(memory).fill([]);
+  }
+
   // Generate a new chain based on XORing each value with its neighbor to the
   // left. If no chain exists, initialize each value with 1.
-  function genChain(chain, size) {
-    for (var c = 0; c < size; c++) {
+  function genChain() {
+    if (grid.length === memory)
+      grid.pop();
+
+    grid.unshift(grid[0].slice(0));  // Make a copy to manipulate
+
+    for (var c = 0; c < gridSize; c++) {
       var x;
-      var leftNeighbor = c === 0 ? size - 1 : c - 1;
-      if (chain[leftNeighbor] === undefined)
+      var leftNeighbor = c === 0 ? gridSize - 1 : c - 1;
+      if (grid[0][leftNeighbor] === undefined)
         x = 1;
       else
-        x = chain[leftNeighbor] ^ chain[c];
-      chain[c] = x;
+        x = grid[0][leftNeighbor] ^ grid[0][c];
+      grid[0][c] = x;
     }
     gen++;
-    $('#md5').text(CryptoJS.MD5(chain.join('')).toString());
+    $('#md5').text(CryptoJS.MD5(grid[0].join('')).toString());
   }
 
   // Update counter and calculate initial/new grid values
   function prepGrid() {
     $('#gen').text(gen);
-    genChain(grid, gridSize);
+    genChain();
   }
 
   // Create grid from scratch with initial values
@@ -60,10 +76,9 @@
       vertexColors: THREE.VertexColors
     });
 
+    var gpos = 0;
     var xpos = -500;
     var ypos = 300;
-
-    var gridCopy = grid.slice(0);
     for (var l = 0; l < 60; l++) {
       for (var p = 0; p < 200; p++) {
         var pX = xpos;
@@ -72,10 +87,10 @@
         var particle = new THREE.Vector3(pX, pY, pZ);
         // add it to the geometry
         particles.vertices.push(particle);
-        var color = gridCopy.shift() ? new THREE.Color(0xffffff) :
-          new THREE.Color(0x000000);
+        var color = getColor(gpos);
         particles.colors.push(color);
         xpos += 5;
+        gpos ++;
       }
       ypos -= 10;
       xpos = -500;
@@ -91,16 +106,36 @@
   function updateGrid() {
     prepGrid();
     particles.colors = [];
-    var gridCopy = grid.slice(0);
+    var gpos = 0;
     for (var l = 0; l < 60; l++) {
       for (var p = 0; p < 200; p++) {
-        var color = gridCopy.shift() ? new THREE.Color(0xffffff) :
-          new THREE.Color(0x000000);
+        var color = getColor(gpos);
         particles.colors.push(color);
         particles.colorsNeedUpdate = true;
+        gpos++;
       }
     }
   }
+
+  function getColor(pos) {
+    var newval = grid[0][pos];
+
+    if (mono)
+      return newval ? new THREE.Color(0xffffff) : new THREE.Color(0x000000);
+
+    var color = 50;
+    for(var i=1;i<grid.length;i++) {
+      var oldval = grid[i][pos];
+      if (oldval === newval)
+        color += 10;
+      else
+        break;
+    }
+    if (grid[0][pos])
+      return new THREE.Color('rgb(0,0,'+color.toString()+')');
+    else
+      return new THREE.Color('rgb('+color.toString()+',0,0)');
+}
 
   function setupClickHandlers() {
     $('#faster').click(function(e) {
@@ -118,7 +153,7 @@
 
     $('#pause').click(function(e) {
       paused = !paused;
-      $('#pause').toggleClass('paused');
+      $('#pause').toggleClass('toggled');
       if (paused)
         $('#pause').text('paused');
       else
@@ -127,7 +162,7 @@
 
     $('#zoom').click(function(e) {
       zoomy = !zoomy;
-      $('#zoom').toggleClass('zooming');
+      $('#zoom').toggleClass('toggled');
       if (zoomy)
         $('#zoom').text('zooming');
       else
@@ -135,9 +170,10 @@
     });
 
     $('#randomize').click(function(e) {
-      grid.forEach(function(ele, ind) {
-        grid[ind] = Math.round(Math.random());
-      });
+      resetGrid();
+      updateGrid();
+      for(var i=0;i<gridSize;i++)
+        grid[0][i] = Math.round(Math.random());
       gen = 1;
       $('#randomized').text(' (randomized)');
       isRandomized = true;
@@ -147,7 +183,7 @@
     });
 
     $('#save').click(function(e) {
-      localStorage.setItem('xorGrid', grid.join(''));
+      localStorage.setItem('xorGrid', grid[0].join(''));
       localStorage.setItem('xorIsRandomized', isRandomized);
       localStorage.setItem('xorGen', gen);
       $('#save').slideUp().slideDown();
@@ -156,7 +192,8 @@
 
     $('#load').click(function(e) {
       var state = localStorage.getItem('xorGrid');
-      grid = state.split('');
+      resetGrid();
+      grid[0] = state.split('');
       gen = Number(localStorage.getItem('xorGen'));
       isRandomized = localStorage.getItem('xorIsRandomized') == 'true' ? true : false;
       if (isRandomized)
@@ -171,6 +208,17 @@
     if (localStorage.getItem('xorGrid')) {
       $('#load').show();
     }
+
+    $('#mono').click(function(e) {
+      if (paused)
+        $('#pause').click();
+      mono = !mono;
+      $('#mono').toggleClass('toggled');
+      if (mono)
+        $('body').append('<iframe id="music" width="100%" hidden scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1245420&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>');
+      else
+        $('#music').remove();
+    });
 
   }
 
